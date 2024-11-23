15cf7e030067b1b147218ee9a212ef58232a2d7d
tyatik <73113510+tyatik@users.noreply.github.com>
2024-11-23 11:58:42 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Link from 'next/link'
+
+// export default function Page() {
+//   return <h1>Hello, Next.js!</h1>
+// }
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <Link href="/page1">Page 1</Link>
 }
